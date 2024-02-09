0d580644f25031adee4bcc96bac814dc5d88d426
jagan <159517250+jagansjd@users.noreply.github.com>
2024-02-09 12:39:00 +0530

    Add files via upload

---
+++ Back-end/spring-boot-library/src/main/java/com/LibraryApp/springbootlibrary/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,11 @@
+package com.LibraryApp.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+}
---
+++ Back-end/spring-boot-library/src/main/java/com/LibraryApp/springbootlibrary/responsemodels/ShelfCurrentLoansResponse.java
@@ -0,0 +1,17 @@
+package com.LibraryApp.springbootlibrary.responsemodels;
+
+import com.LibraryApp.springbootlibrary.entity.Book;
+import lombok.Data;
+
+@Data
+public class ShelfCurrentLoansResponse {
+
+    public ShelfCurrentLoansResponse(Book book, int daysLeft) {
+        this.book = book;
+        this.daysLeft = daysLeft;
+    }
+
+    private Book book;
+
+    private int daysLeft;
+}
